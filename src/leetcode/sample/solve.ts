29421320dbc15e6558a2f5cf50f016140c7f3af6
Your Name <you@example.com>
2025-03-11 21:13:08 +1100

    sample

---
+++ src/leetcode/sample/solve.ts
@@ -0,0 +1,5 @@
+export class Solver {
+    static aPlusB(a: number, b: number): number {
+        return a + b;
+    };
+}
---
+++ src/leetcode/sample/test.ts
@@ -0,0 +1,7 @@
+import {Solver} from './solve';
+describe('aPlusB', () => {
+   it('first', () => {
+       expect(Solver.aPlusB(1, 2))
+           .toBe(3)
+   });
+});
